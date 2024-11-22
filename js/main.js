21c89313b2b68dc9bbb1929da4ee0eec0600969a
Emilie <emilie.chnvt@gmail.com>
2024-11-22 15:45:55 +0100

    chat added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 const socket = io("ws://10.9.73.20:8080")
 const button = document.querySelector("button")
 
+
+socket.on('message', (message) => {
+    console.log(message)
+    const li = document.createElement("li")
+    li.innerHTML = message.author + " a ecrit :" + message.content
+    document.querySelector("ul").appendChild(li)
+
+})
+
+
+button.addEventListener("click", (e) => {
+    const toSend = document.querySelector("input").value
+    socket.emit('message',{
+        content : toSend,
+    })
+})
+
+
+
